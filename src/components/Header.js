dfe91076b0845b7da35e5d74eeeffe77531d32c5
Tatiana Bialik <bialik.tat@gmail.com>
2022-09-18 21:59:55 +0300

    feat: mobile design add

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, NavLink } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
 function Header({ email, onLogout }) {
   const location = useLocation();
+  const [isBurgerButtonActive, setIsBurgerButtonActive] = useState(true);
+
+  function handleBurgerMenuClick() {
+    setIsBurgerButtonActive(true);
+  }
+
+  function handleCloseMenuClick() {
+    setIsBurgerButtonActive(false);
+  }
 
   return (
     <header className="header">
-      <img 
-      src={logo}
-      alt="Around the U.S. inscription" 
-      className="header__logo" />
-
-      {location.pathname === '/signin' && (<NavLink to='/signup' className='header__item link'>Sign up</NavLink>)}
-      {location.pathname === '/signup' && (<NavLink to='/signin' className='header__item link'>Log in</NavLink>)}
-      {location.pathname === '/' && (
-        <div className="header__menu">
+      {isBurgerButtonActive && (
+        <div className="header__menu_active">
           <p className='header__item header__text'>{email}</p>
           <button className="header__item header__button action-button" onClick={onLogout}>Log out</button>
-        </div>
-        )}
+      </div>
+      )}
+      <div className="header__main">
+        <img 
+        src={logo}
+        alt="Around the U.S. inscription" 
+        className="header__logo" />
+
+        {location.pathname === '/signin' && (<NavLink to='/signup' className='header__item link'>Sign up</NavLink>)}
+        {location.pathname === '/signup' && (<NavLink to='/signin' className='header__item link'>Log in</NavLink>)}
+        {location.pathname === '/' && (
+          <div className="header__navbar">
+            {/* {isBurgerButtonActive ? (
+            
+            ) : (
+              <button 
+              className='header__burger-menu action-button'
+              onClick={handleBurgerMenuClick} />
+            )} */}
+            {/* <button 
+            className='header__burger-menu action-button'
+            onClick={handleBurgerMenuClick} />
+            <div className="header__menu">
+              <p className='header__item header__text'>{email}</p>
+              <button className="header__item header__button action-button" onClick={onLogout}>Log out</button>
+            </div> */}
+          </div>
+          )}
+      </div>
     </header>
   )
 }
